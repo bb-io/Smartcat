ab42fd8e7334689e8765cff904dc91ffff039f1d
bZverok <137277669+bZverok@users.noreply.github.com>
2024-05-27 11:43:59 +0300

    added connection validator

--- Smartcat/Connections/ConnectionValidator.cs
+++ Smartcat/Connections/ConnectionValidator.cs
@@ -1,5 +1,23 @@
-﻿namespace Smartcat.Connections;
+﻿using Apps.Smartcat.API;
+using Apps.Smartcat.Constants;
+using Blackbird.Applications.Sdk.Common.Authentication;
+using Blackbird.Applications.Sdk.Common.Connections;
+using RestSharp;
 
-internal class ConnectionValidator
+namespace Apps.Smartcat.Connections;
+
+internal class ConnectionValidator : IConnectionValidator
 {
+    public async ValueTask<ConnectionValidationResponse> ValidateConnection(IEnumerable<AuthenticationCredentialsProvider> authProviders, CancellationToken cancellationToken)
+    {
+        var client = new SmartcatClient();
+        
+        var request = new SmartcatRequest(Urls.Api + "project/list", Method.Get, authProviders);
+        await client.ExecuteWithHandling(request);
+
+        return new()
+        {
+            IsValid = true
+        };
+    }
 }
